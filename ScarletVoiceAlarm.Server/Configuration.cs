ae437abbb3abd41633a1c3db4b3362528c6d7a0a
joyrax-dev <joyrax.dev@gmail.com>
2024-03-24 22:02:49 +1000

    Добавьте файлы проекта.

---
+++ ScarletVoiceAlarm.Server/Configuration.cs
@@ -0,0 +1,109 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Windows.Input;
+
+namespace ScarletVoiceAlarm.Server
+{
+    #region Types
+    internal class Config
+    {
+        [JsonPropertyName("Listen")]
+        public Listen Listen { get; set; }
+
+        [JsonPropertyName("KeyBindings")]
+        public KeyBindings[] KeyBindings { get; set; }
+
+        [JsonPropertyName("Locations")]
+        public Locations[] Locations { get; set; }
+
+        public Config(Listen listen, KeyBindings[] keyBindings, Locations[] locations) 
+        {
+            Listen = listen;
+            KeyBindings = keyBindings;
+            Locations = locations;
+        }
+    }
+
+    internal class Listen
+    {
+        [JsonPropertyName("Host")]
+        public string Host { get; set; }
+
+        [JsonPropertyName("Port")]
+        public int Port { get; set; }
+
+        public Listen(string host, int port)
+        {
+            Host = host;
+            Port = port;
+        }
+    }
+
+    internal class KeyBindings
+    {
+        [JsonPropertyName("Key")]
+        public string Key { get; set; }
+
+        [JsonPropertyName("Locations")]
+        public string[] Locations { get; set; }
+
+        [JsonIgnore]
+        public Key SystemKey { get; set; }
+
+        public KeyBindings(string key, string[] locations)
+        {
+            Key = key;
+            Locations = locations;
+
+            try
+            {
+                Enum.TryParse(Key, out Key systemKey);
+                SystemKey = systemKey;
+            }
+            catch
+            {
+                Console.WriteLine("[Error] Bad key value: " + Key);
+            }
+        }
+    }
+
+    internal class Locations
+    {
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("Host")]
+        public string Host { get; set; }
+
+        [JsonPropertyName("Port")]
+        public int Port { get; set; }
+
+        [JsonIgnore]
+        public IPEndPoint EndPoint { get; set; }
+
+        public Locations(string name, string host, int port)
+        {
+            Name = name;
+            Host = host;
+            Port = port;
+            EndPoint = new IPEndPoint(IPAddress.Parse(Host), Port);
+        }
+    }
+    #endregion
+
+    internal static class Configuration
+    {
+        public static Config? Config { get; set; }
+
+        public static void Load()
+        {
+            using (FileStream fs = new FileStream("config.json", FileMode.OpenOrCreate))
+            {
+                Config? config = JsonSerializer.Deserialize<Config>(fs);
+
+                Config = config;
+            }
+        }
+    }
+}
---
+++ ScarletVoiceAlarm.Server/Program.cs
@@ -0,0 +1,72 @@
+﻿using KeyboardHookLite;
+using NAudio.Wave;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Input;
+
+namespace ScarletVoiceAlarm.Server
+{
+    internal class Program
+    {
+        private static Key? PressedKey = null;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Load Configuration...");
+            Configuration.Load();
+
+            KeyBindings[]? bindings = Configuration.Config?.KeyBindings;
+            for (int i = 0; i < bindings?.Length; i++)
+            {
+                KeyBindings binding = bindings[i];
+                Console.WriteLine("Key: " + binding.SystemKey);
+            }
+            Console.WriteLine("+++");
+
+            WaveInEvent waveIn = new WaveInEvent();
+            waveIn.DeviceNumber = 0;
+            waveIn.DataAvailable += OnRecordData;
+            waveIn.StartRecording();
+
+            KeyboardHook kbh = new KeyboardHook();
+            kbh.KeyboardPressed += OnKeyPress;
+            System.Windows.Threading.Dispatcher.Run();
+
+            Console.ReadLine();
+        }
+
+        private static void OnRecordData(object? sender, WaveInEventArgs e)
+        {
+            KeyBindings[]? bindings = Configuration.Config?.KeyBindings;
+            for (int i = 0; i < bindings?.Length; i++)
+            {
+                KeyBindings binding = bindings[i];
+                
+                if (binding.SystemKey == PressedKey)
+                {
+                    Console.Write("Sended voice to [");
+                    foreach (string loc in binding.Locations)
+                    {
+                        Console.Write(" " + loc + " ");
+                    }
+                    Console.Write("]\n");
+                }
+            }
+        }
+
+        private static void OnKeyPress(object? sender, KeyboardHookEventArgs e)
+        {
+            if (e.KeyPressType == KeyboardHook.KeyPressType.KeyDown)
+            {
+                PressedKey = e.InputEvent.Key;
+            }
+            else
+            {
+                PressedKey = null;
+            }
+            
+        }
+    }
+}
